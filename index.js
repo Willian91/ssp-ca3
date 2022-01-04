e523dedaa2b270f5dbc53f13304260f8c368b2da
Willian Amaral <will.yeah@hotmail.com>
2022-01-04 01:27:46 +0000

    1.4

---
+++ index.js
@@ -0,0 +1,7 @@
+const http = require('http'),
+    path = requeire('path'),
+    express = require('express'),
+    fs = require('fs'),
+    xmlParse = require('xslt-processor').xmlParse,
+    xmlProcxess = require('xslt-processor').xmlProcxess,
+    xml2js = require('xml2js')
