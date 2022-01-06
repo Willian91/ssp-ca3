0f6c4e32d54743939f8542ae6114fb75785e1faf
Willian Amaral <66034862+Willian91@users.noreply.github.com>
2022-01-06 17:14:03 +0000

    update
    format some coments and router

--- index.js
+++ index.js
@@ -1,56 +1,64 @@
-const express = require('express'), //Allows to respond to HTTP requests, defines routing and renders the required content
-    fs = require('fs'), //Working with the file system (read and write files)
-    http = require('http'), //HTTP Server
-    path = require('path'), //Utility that allows us to work with directory paths
-    xml2js = require('xml2js'), //This is XML <-> JSON converter
-    xmlParse = require('xslt-processor').xmlParse, //Parsing XML
-    xsltProcess = require('xslt-processor').xsltProcess; //Processing XSLT
-
-const router = express(), //Instantiating Express
-    server = http.createServer(router); //Instantiating the server
-
-router.use(express.static(path.resolve(__dirname, 'views'))); //Serving static content from "views" folder
-router.use(express.urlencoded({extended: true}));
+//Allows to respond to HTTP requests, defines routing and renders the required content
+const express = require('express'),
+//Working with the file system (read and write files)
+const fs = require('fs'),
+//HTTP Server
+const http = require('http'),
+//Utility that allows us to work with directory paths
+const path = require('path'),
+//This is XML <-> JSON converter
+const xml2js = require('xml2js'),
+//Parsing XML
+const xmlParse = require('xslt-processor').xmlParse,
+//Processing XSLT
+const xsltProcess = require('xslt-processor').xsltProcess;
+//Instantiating Express
+const router = express(),
+//Instantiating the server
+const server = http.createServer(router);
+//Serving static content from "views" folder
+router.use(express.static(path.resolve(__dirname, 'views')));
+router.use(express.urlencoded({ extended: true }));
 router.use(express.json());
 
 function XMLtoJSON(filename, cb) {
-    let filepath = path.normalize(path.join(__dirname, filename));
-    fs.readFile(filepath, 'utf8', function(err, xmlStr) {
+    const filepath = path.normalize(path.join(__dirname, filename));
+    fs.readFile(filepath, 'utf8', function (err, xmlStr) {
         if (err) throw (err);
         xml2js.parseString(xmlStr, {}, cb);
     });
 };
 
 function JSONtoXML(filename, obj, cb) {
-    let filepath = path.normalize(path.join(__dirname, filename));
-    let builder = new xml2js.Builder();
-    let xml = builder.buildObject(obj);
+    const filepath = path.normalize(path.join(__dirname, filename));
+    const builder = new xml2js.Builder();
+    const xml = builder.buildObject(obj);
     fs.unlinkSync(filepath);
     fs.writeFile(filepath, xml, cb);
 };
 
-router.get('/get/html', function(req, res) {
+router.get('/html', function (req, res) {
 
-    res.writeHead(200, { 'Content-Type': 'text/html' }); //Tell the user that the resource exists and which type that is
+    //Tell the user that the resource exists and which type that is
+    res.writeHead(200, { 'Content-Type': 'text/html' });
+    //read in the XML file
+    const xml = fs.readFileSync('WillianCafe.xml', 'utf8'),
+        //read in the XSL file
+        xsl = fs.readFileSync('WillianCafe.xsl', 'utf8');
 
-    let xml = fs.readFileSync('WillianCafe.xml', 'utf8'), //read in the XML file
-        xsl = fs.readFileSync('WillianCafe.xsl', 'utf8'); //read in the XSL file
+    //Parse the XML file
+    const doc = xmlParse(xml),
+        //Parse the XSL file
+        stylesheet = xmlParse(xsl);
+    //Performing XSLT
+    const result = xsltProcess(doc, stylesheet);
+    //Serve back the user
 
-    // console.log(xml);
-    // console.log(xsl);
-
-    let doc = xmlParse(xml), //Parse the XML file
-        stylesheet = xmlParse(xsl); //Parse the XSL file
-
-    let result = xsltProcess(doc, stylesheet); //Performing XSLT
-
-    // console.log(result);
-
-    res.end(result.toString()); //Serve back the user
+    res.end(result.toString());
 
 });
 
-router.post('/post/json', function(req, res) {
+router.post('/json', function (req, res) {
 
     console.log(req.body);
 
@@ -58,14 +66,14 @@ router.post('/post/json', function(req, res) {
 
         console.log(JSON.stringify(obj, null, " "))
 
-        XMLtoJSON('WillianCafe.xml', function(err, result) {
+        XMLtoJSON('WillianCafe.xml', function (err, result) {
             if (err) throw (err);
 
             result.menu.section[obj.sec_n].entry.push({ 'item': obj.item, 'price': obj.price });
 
             console.log(JSON.stringify(result, null, " "));
 
-            JSONtoXML('WillianCafe.xml', result, function(err) {
+            JSONtoXML('WillianCafe.xml', result, function (err) {
                 if (err) console.log(err);
             });
 
@@ -79,7 +87,7 @@ router.post('/post/json', function(req, res) {
 
 });
 
-router.post('/post/delete', function(req, res) {
+router.post('/post/delete', function (req, res) {
 
     console.log(req.body);
 
@@ -87,7 +95,7 @@ router.post('/post/delete', function(req, res) {
 
         console.log(obj)
 
-        XMLtoJSON('WillianCafe.xml', function(err, result) {
+        XMLtoJSON('WillianCafe.xml', function (err, result) {
             if (err) throw (err);
 
             console.log(obj.sec);
@@ -96,7 +104,7 @@ router.post('/post/delete', function(req, res) {
 
             delete result.menu.section[obj.sec].entry[obj.ent];
 
-            JSONtoXML('WillianCafe.xml', result, function(err) {
+            JSONtoXML('WillianCafe.xml', result, function (err) {
                 if (err) console.log(err);
             });
         });
@@ -108,7 +116,7 @@ router.post('/post/delete', function(req, res) {
 
 });
 
-server.listen(process.env.PORT || 3000, process.env.IP || "0.0.0.0", function() {
+server.listen(process.env.PORT || 3000, process.env.IP || "0.0.0.0", function () {
     const addr = server.address();
     console.log('Server listening at', addr.address + ':' + addr.port)
 });
--- views/js/table.js
+++ views/js/table.js
@@ -12,7 +12,7 @@ function draw_table(){
         });
 
     };
-    $.getHTMLuncached("/get/html");
+    $.getHTMLuncached("/html");
 };
 
 
